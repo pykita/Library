97306d0101d9067a44de41ed92746460072cacd5
Tordingo <nikbolotin@yandex.ua>
2015-05-28 22:28:13 +0300

    initial commit

---
+++ Library.Factory/Factories/BooksFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Library.Factory.Contexts;
+using Library.Factory.Models;
+
+namespace Library.Factory.Factories
+{
+    public static class BooksFactory
+    {
+        public static List<Book> GetList()
+        {
+            using (var db = new BookContext())
+            {
+                return db.Books.ToList();
+            }
+
+        }
+
+        public static Book AddBook(Book newBook)
+        {
+            using (var db = new BookContext())
+            {
+                var book = db.Books.Add(newBook);
+                db.SaveChanges();
+
+                return book;
+            }
+        } 
+    }
+}
---
+++ Library/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Library.Factory.Models;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void btnHelloWorld_Click(object sender, RoutedEventArgs e)
+        {
+            Factory.Factories.BooksFactory.AddBook(new Book { Name = "Harry Potter and phylosophy stone" });
+            Factory.Factories.BooksFactory.AddBook(new Book { Name = "Harry Potter and chamber of secrets" });
+
+            var items = Factory.Factories.BooksFactory.GetList();
+        }
+    }
+}
